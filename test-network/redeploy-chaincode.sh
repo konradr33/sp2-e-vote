8a0ebe9eca1627f9836c103753e59fe2c33e0ffc
Konrad Radoń <konradradon@gmail.com>
2020-12-16 23:36:42 +0100

    Add plain application, adjust setup script.

--- test-network/redeploy-chaincode.sh
+++ test-network/redeploy-chaincode.sh
@@ -2,6 +2,6 @@ export CC_NAME="e-vote";
 export CC_LANGUAGE="javascript";
 
 ./network.sh down
-./network.sh up
+./network.sh up -ca
 ./network.sh createChannel
 ./network.sh deployCC -ccn $CC_NAME -ccl $CC_LANGUAGE
